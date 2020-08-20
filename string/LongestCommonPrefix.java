2edc083451ffe61ba7b7428612689085bf20c2b4
julia wu <julia.wu@sap.com>
2020-08-20 18:13:36 +0800

    add string folder

---
+++ string/LongestCommonPrefix.java
@@ -0,0 +1,32 @@
+package string;
+//14. 最长公共前缀
+class LongestCommonPrefix {
+    public String longest(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        String prefix = strs[0];
+        int count = strs.length;
+        for (int i = 1; i < count; i++) {
+            prefix = longestCommonPrefix(prefix, strs[i]);
+            if (prefix.length() == 0) {
+                break;
+            }
+        }
+        return prefix;
+    }
+
+    public String longestCommonPrefix(String str1, String str2) {
+        int length = Math.min(str1.length(), str2.length());
+        int index = 0;
+        while (index < length && str1.charAt(index) == str2.charAt(index)) {
+            index++;
+        }
+        return str1.substring(0, index);
+    }
+
+    public static void main(String[] args) {
+        String[] s = new String[]{"abc", "abd"};
+        System.out.println(new LongestCommonPrefix().longest(s));
+    }
+}
